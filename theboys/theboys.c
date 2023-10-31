81c3b8779967eaadab55bafe4c233956da340e68
EDUARDO FARIA KRUGER <efk23@inf.ufpr.br>
2023-10-31 15:15:08 -0300

    primeiro commit

---
+++ theboys/theboys.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* demais includes */
+
+/* funcoes que voce ache necessarias aqui */
+
+int main (){
+    /* declaracoes de variaveis aqui */
+
+    srand (0); /* use zero, nao faca com time (0) */
+
+    /* coloque seu codigo aqui */
+
+    return 0;
+}
